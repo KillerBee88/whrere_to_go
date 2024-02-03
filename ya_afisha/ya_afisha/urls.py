414c652a4832c63e3829af07bee30a112b2f8db2
KillerBee88 <87515094+KillerBee88@users.noreply.github.com>
2024-02-03 20:26:12 +0300

    Delete urls.py

--- ya_afisha/ya_afisha/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from ya_afisha.views import where_to_go
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', where_to_go),
-]
