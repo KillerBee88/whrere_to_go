3e1bbcd53d4275f903f4f0e2b2d0c0a51671329f
KillerBee88 <87515094+KillerBee88@users.noreply.github.com>
2024-02-08 20:52:34 +0300

    Update settings.py
    Setup media files serving

--- ya_afisha/settings.py
+++ ya_afisha/settings.py
@@ -90,3 +90,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'ya_afisha/static'),]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
