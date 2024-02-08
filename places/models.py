fe11d6844a3ca75da17e3816a12fe7833e2d8f83
KillerBee88 <87515094+KillerBee88@users.noreply.github.com>
2024-02-08 20:03:49 +0300

    Update models.py
    add Places model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Place(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
 
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
