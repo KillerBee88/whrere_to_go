10efafb7236e09baf8c0ba798927b23c99ff1a8b
KillerBee88 <87515094+KillerBee88@users.noreply.github.com>
2024-02-08 20:52:39 +0300

    Update models.py
    Setup media files serving

--- places/models.py
+++ places/models.py
@@ -10,3 +10,15 @@ class Place(models.Model):
     class Meta:
         verbose_name = 'Место'
         verbose_name_plural = 'Места'
+
+class Image(models.Model):
+    location = models.ForeignKey(Place, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images')
+    order = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.image.url
+
+    class Meta:
+        verbose_name = 'Фотография'
+        verbose_name_plural = 'Фотографии'
