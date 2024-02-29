665d8116735679fd1a96d7bf7cc197429eb6be4d
KillerBee88 <87515094+KillerBee88@users.noreply.github.com>
2024-02-29 21:32:27 +0300

    Update admin.py
    упростил добавление фотографий

--- places/admin.py
+++ places/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Place, Image
 
-admin.site.register(Place)
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 3
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
+
 admin.site.register(Image)
