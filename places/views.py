eee54a3630f71ab4a042eef17dd651a06e033705
KillerBee88 <87515094+KillerBee88@users.noreply.github.com>
2024-02-29 21:01:38 +0300

    Update views.py

--- places/views.py
+++ places/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from places.models import Place
 
-# Create your views here.
+
+def show_mainpage(request):
+    places = Place.objects.all()
+    features = []
+    for place in places:
+        feature = {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [place.longitude, place.latitude]
+            },
+            'properties': {
+                'title': place.title,
+                'placeId': place.id,
+                'detailsUrl': f'/places/{place.id}'
+            }
+        }
+        features.append(feature)
+
+    geo_json = {
+        'type': 'FeatureCollection',
+        'features': features
+    }
+    return render(request, 'main_page.html', context={'geo_json': geo_json})
+
+def show_place(request, place_id):
+    place = get_object_or_404(Place, id=place_id)
+    place_data = {
+        'title': place.title,
+        'imgs': [image.image.url for image in place.images.all()],
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lng': place.longitude,
+            'lat': place.latitude
+        }
+    }
+    return JsonResponse(place_data, safe=False, 
+                        json_dumps_params={'ensure_ascii': False, 
+                                           'indent': 4})
