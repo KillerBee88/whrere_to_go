df260609a97e632b39eaefe763b2cc89ed3f3eb8
KillerBee88 <87515094+KillerBee88@users.noreply.github.com>
2024-03-01 22:27:44 +0300

    Update admin.py
    Добавлен WYSIWYG-редактор django-tinymce для текстовых полей в админке

--- places/admin.py
+++ places/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from adminsortable2.admin import SortableInlineAdminMixin, SortableAdminBase
 from .models import Place, Image
 
-class ImageInline(admin.TabularInline):
+class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
     model = Image
     extra = 3
     readonly_fields = ['image_preview']
-    fields = ('image', 'image_preview',)
+    fields = ('image', 'image_preview', 'order')
 
     def image_preview(self, obj):
         return format_html('<img src="{}" style="max-height: 200px;"/>', obj.image.url) if obj.image else ''
@@ -14,5 +15,5 @@ class ImageInline(admin.TabularInline):
     image_preview.short_description = "Превью"
 
 @admin.register(Place)
-class PlaceAdmin(admin.ModelAdmin):
+class PlaceAdmin(SortableAdminBase,admin.ModelAdmin):
     inlines = [ImageInline]
