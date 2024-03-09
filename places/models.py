f5f2ddb8ae6d9330806fa328f4d73e89423de333
KillerBee88 <87515094+KillerBee88@users.noreply.github.com>
2024-03-09 10:53:08 +0300

    Update models.py

--- places/models.py
+++ places/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from tinymce.models import HTMLField
 
 class Place(models.Model):
-    title = models.CharField(max_length=200)
-    description_short = models.TextField(null=True, blank=True)
-    description_long = HTMLField(null=True, blank=True)
-    latitude = models.FloatField(null=True, blank=True)
-    longitude = models.FloatField(null=True, blank=True)
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    description_short = models.TextField(null=True, blank=True, verbose_name='Краткое описание')
+    description_long = HTMLField(null=True, blank=True, verbose_name='Полное описание')
+    latitude = models.FloatField(null=True, blank=True, verbose_name='Широта')
+    longitude = models.FloatField(null=True, blank=True, verbose_name='Долгота')
 
     def get_coordinates(self):
         return [self.latitude, self.longitude]
@@ -19,9 +19,9 @@ class Place(models.Model):
         verbose_name_plural = 'Места'
 
 class Image(models.Model):
-    location = models.ForeignKey(Place, related_name='images', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='images')
-    order = models.IntegerField(default=0)
+    location = models.ForeignKey(Place, related_name='images', on_delete=models.CASCADE, verbose_name='Местоположение')
+    image = models.ImageField(upload_to='images', verbose_name='Изображение')
+    order = models.IntegerField(default=0, verbose_name='Порядок')
 
     def __str__(self):
         return f"{self.order}: {self.image.url}"
